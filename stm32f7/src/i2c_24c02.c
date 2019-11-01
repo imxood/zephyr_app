87ac722f9a2afd859c5a531d15540be1832c3ad9
hello <Imxood@gmail.com>
2019-11-01 12:19:28 +0800

    Update i2c_24c02.c

--- stm32f7/src/i2c_24c02.c
+++ stm32f7/src/i2c_24c02.c
@@ -12,109 +12,88 @@ static struct device *i2c_dev;
 
 static u32_t i2c_cfg = I2C_SPEED_SET(I2C_SPEED_STANDARD) | I2C_MODE_MASTER;
 
-int i2c_24c02_read(struct device *dev, uint8_t start_addr, uint8_t *data, uint8_t len)
+
+void test_zephyr_i2c_write_read()
 {
-    // 1010(固定值) 000(A2A1A0)0(R:1,W:0)
-    uint8_t addrs[] = {0xa0, start_addr};
+	const char *devname = "I2C_2";
+	struct device *i2c_dev = device_get_binding(devname);
+	if (i2c_dev == NULL) {
+		printk("can't get ic2_dev['%s']\n", devname);
+		return;
+	}
 
-    struct i2c_msg msgs[3];
-    uint8_t addr_read = 0xa1;
+	u32_t i2c_cfg = I2C_SPEED_SET(I2C_SPEED_FAST) | I2C_MODE_MASTER;
 
-    // write: device write address, device word address
-    msgs[0].buf = addrs;
-    msgs[0].len = 2;
-    msgs[0].flags = I2C_MSG_WRITE;
+	if (i2c_configure(i2c_dev, i2c_cfg)) {
+		printk("I2C config Fail.\n");
+		return;
+	}
 
-    // write: device read address
-    msgs[1].buf = &addr_read;
-    msgs[1].len = 1;
-    msgs[1].flags = I2C_MSG_WRITE;
+    k_sleep(100);
 
-    // read data
-    msgs[2].buf = data;
-    msgs[2].len = len;
-    msgs[2].flags = I2C_MSG_READ | I2C_MSG_RESTART;
+	uint8_t buf[5] = {0, 0, 1, 2, 3};
 
-    return i2c_transfer(dev, msgs, 3, I2C_SLAVE_ADDR);
-}
+	if (i2c_write(i2c_dev, buf, 5, 0x50)) {
+		printk("i2c_write Fail.\n");
+		return;
+	}
+    k_sleep(100);
 
-int i2c_24c02_write(struct device *dev, uint8_t start_addr, uint8_t *data, uint8_t len)
-{
-    // 1010(固定值) 000(A2A1A0)0(R:1,W:0)
-    uint8_t addrs[] = {0xa0, start_addr};
+	printk("write data:\n");
+	hexdump(buf+1, 4);
 
-    struct i2c_msg msgs[2];
-    uint8_t addr_read = 0xa1;
+	if (i2c_write(i2c_dev, buf, 1, 0x50)) {
+		printk("i2c_write Fail.\n");
+		return;
+	}
 
-    // write: device write address, device word address
-    msgs[0].buf = addrs;
-    msgs[0].len = 2;
-    msgs[0].flags = I2C_MSG_WRITE;
+    k_sleep(100);
 
-    // write: device read address
+	if (i2c_read(i2c_dev, buf + 1, 4, 0x50)) {
+		printk("i2c_read Fail.\n");
+		return;
+	}
 
-    // write data
-    msgs[1].buf = data;
-    msgs[1].len = len;
-    msgs[1].flags = I2C_MSG_WRITE | I2C_MSG_RESTART;
+    k_sleep(100);
 
-    return i2c_transfer(dev, msgs, 2, I2C_SLAVE_ADDR);
-}
+	printk("read data:\n");
+	hexdump(buf+1, 4);
 
-void test_i2c_24c02(void)
-{
-    int32_t ret = 0;
-    uint8_t buf[256] = {0};
 
-    printk("%s[%d]\n", __func__, __LINE__);
-    i2c_dev = device_get_binding(I2C_DEV_NAME);
-    if (i2c_dev == NULL)
-    {
-        printk("can't get i2c device[%s]\n", I2C_DEV_NAME);
-        return;
-    }
+    buf[1] = 2;
+	buf[2] = 3;
+	buf[3] = 4;
+    buf[4] = 5;
 
-    printk("%s[%d]\n", __func__, __LINE__);
-    ret = i2c_configure(i2c_dev, i2c_cfg);
-    if (ret) {
-        printk("run i2c_configure failed, ret: %d\n", ret);
-        return;
-    }
+	if (i2c_write(i2c_dev, buf, 5, 0x50)) {
+		printk("i2c_write Fail.\n");
+		return;
+	}
+	printk("write data:\n");
+	hexdump(buf+1, 4);
 
-    printk("%s[%d]\n", __func__, __LINE__);
-    ret = i2c_24c02_read(i2c_dev, 0x00, buf, 256);
-    if (ret) {
-        printk("run i2c_24c02_read failed, ret: %d\n", ret);
-        return;
-    }
-    hexdump(buf, 256);
+    k_sleep(100);
 
-    for (int i = 0; i < 256; i++) {
-        buf[i] = i;
-    }
+	if (i2c_write(i2c_dev, buf, 1, 0x50)) {
+		printk("i2c_write Fail.\n");
+		return;
+	}
 
-    printk("%s[%d]\n", __func__, __LINE__);
-    ret = i2c_24c02_write(i2c_dev, 0x00, buf, 256);
-    if (ret) {
-        printk("run i2c_24c02_write failed, ret: %d\n", ret);
-        return;
-    }
+    k_sleep(100);
 
-    memset(buf, 0, 256);
+	if (i2c_read(i2c_dev, buf + 1, 4, 0x50)) {
+		printk("i2c_read Fail.\n");
+		return;
+	}
 
-    printk("%s[%d]\n", __func__, __LINE__);
-    ret = i2c_24c02_read(i2c_dev, 0x00, buf, 256);
-    if (ret) {
-        printk("run i2c_24c02_read failed, ret: %d\n", ret);
-        return;
-    }
-    hexdump(buf, 256);
+	printk("read data:\n");
+	hexdump(buf+1, 4);
 }
 
 void e24c02_service(struct device *unused)
 {
     ARG_UNUSED(unused);
-    test_i2c_24c02();
+    test_zephyr_i2c_write_read();
 }
 
 SYS_INIT(e24c02_service, APPLICATION, 90);
